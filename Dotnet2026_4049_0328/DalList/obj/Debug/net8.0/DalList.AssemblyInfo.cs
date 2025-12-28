385be51c3aead7647d2f8dde242d5fde476fe5ae
tovi1892 <tovi1892@gmail.com>
2025-12-28 16:41:31 +0200

    הודעת commit שלך

--- Dotnet2026_4049_0328/DalList/obj/Debug/net8.0/DalList.AssemblyInfo.cs
+++ Dotnet2026_4049_0328/DalList/obj/Debug/net8.0/DalList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DalList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838d7508c7d45ae8b412304ec05f9fbc4d87d686")]
 [assembly: System.Reflection.AssemblyProductAttribute("DalList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DalList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
