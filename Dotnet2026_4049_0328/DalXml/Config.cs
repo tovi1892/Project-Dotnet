02ba90cb65a0c081da8c234eda1a3d4cef1351f6
tovi1892 <tovi1892@gmail.com>
2026-03-11 19:47:49 +0200

    level 9

---
+++ Dotnet2026_4049_0328/DalXml/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalXml;
+
+static  internal class Config
+{
+    static private string fileName= "data-config";
+    static private int productNextId;
+
+    static public int MyProductNextId
+    {
+		get { return productNextId; }
+		 
+	}
+
+
+}
